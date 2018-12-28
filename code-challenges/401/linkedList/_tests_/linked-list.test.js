bbe344ebc67e0ba49d662c068df4178a468c5ac1
Tanner Seramur <seramurt@gmail.com>
2018-12-27 20:31:15 -0800

    finished w/ tests

--- code-challenges/401/linkedList/_tests_/linked-list.test.js
+++ code-challenges/401/linkedList/_tests_/linked-list.test.js
@@ -3,8 +3,25 @@ const LinkedList = require ('../linked-list.js');
 
 describe('running tests', ()=>{
     it('can add a item to list using insert function', () => {
-        let list = new LinkedList()
+        let list = new LinkedList();
         list.insert('tanner')
         expect(list.head.value).toEqual('tanner');
+    });
+    it(`can add a new value before the entered value in the list using insertBefore`,()=>{
+        let list = new LinkedList();
+        list.insert('tanner');
+        list.insert('maria');
+        list.insertBefore('maria','kona');
+        expect(list.includes('kona')).toEqual(true);
+    });
+    it(`can add a new value after the entered value in the list using insertAfter`,()=>{
+        let list = new LinkedList();
+        list.insert('tanner');
+        list.insert('maria');
+        list.insert('ryli');
+        list.insertAfter('maria','kona');
+        expect(list.includes('kona')).toEqual(true);
     })
+
+
 })
