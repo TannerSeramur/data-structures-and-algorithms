daee472105eb8974fd0da234fa1aa16c795a4f2c
Tanner Seramur <seramurt@gmail.com>
2019-01-14 08:16:42 -0800

    finished tree

--- code-challenges/401/stacks-and-queues/stacks-and-queues.js
+++ code-challenges/401/stacks-and-queues/stacks-and-queues.js
@@ -30,7 +30,30 @@ class Stack {
   peek(){
     return this.top.value;
   }
+  getMax(){
+    let bigNum = 0;
+    let current = this.top;
+    while(current.next){
+      console.log(current, ' ⭐️ ');
+      
+      if(current.value > bigNum){
+        bigNum = current.value;
+      }
+      current = current.next;
+    }
+    // console.log(bigNum);
+    
+    return bigNum;
 
 }
-module.exports = Stack;
+}
+// module.exports = Stack;
+
+let test = new Stack();
+test.push(1);
+test.push(2);
+test.push(4);
+test.push(2);
+test.push(1);
+test.getMax();
 
